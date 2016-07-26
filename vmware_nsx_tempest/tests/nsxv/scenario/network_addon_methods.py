stackforge/vmware-nsx

Tempest: Providing support for disable spoofguard feature
Added scenario testcase for disable spoofguard feature.

Change-Id: Ifd741e7fbc4a5fa6a577864bdf98eaad9082b876

vmware_nsx_tempest/tests/nsxv/scenario/network_addon_methods.py
@@ -178,6 +178,19 @@ def create_network(SELF, client=None, tenant_id=None, name=None, **kwargs):
     return net_network
 
 
+def create_port_args(SELF, client=None, **kwargs):
+        if not client:
+            client = SELF.port_client
+        result = client.create_port(**kwargs)
+        net_port = result['port']
+        SELF.assertIsNotNone(result, 'Unable to allocate port')
+        SELF.addCleanup(test_utils.call_and_ignore_notfound_exc,
+                        client.delete_port,
+                        net_port['id'])
+
+        return net_port
+
+
 # gateway=None means don't set gateway_ip in subnet
 def create_subnet(SELF, network, client=None,
                   gateway='', cidr=None, mask_bits=None,

openstack-archive/vmware-nsx-tempest-plugin

vmware_nsx_tempest/tests/nsxv/scenario/network_addon_methods.py
@@ -178,6 +178,19 @@ def create_network(SELF, client=None, tenant_id=None, name=None, **kwargs):
     return net_network
 
 
+def create_port(SELF, client=None, **kwargs):
+        if not client:
+            client = SELF.port_client
+        result = client.create_port(**kwargs)
+        net_port = result['port']
+        SELF.assertIsNotNone(result, 'Unable to allocate port')
+        SELF.addCleanup(test_utils.call_and_ignore_notfound_exc,
+                        client.delete_port,
+                        net_port['id'])
+
+        return net_port
+
+
 # gateway=None means don't set gateway_ip in subnet
 def create_subnet(SELF, network, client=None,
                   gateway='', cidr=None, mask_bits=None,