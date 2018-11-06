34e66c73a728a78652d31c594304aecb28e989ea
Shubhamk Kadam <shubhamk@vmware.com>
2018-11-06 13:14:07 +0000

    [Tempest]: Adding barbican cases Check HTTPS traffic with brabican secrets and containers Check cert and key updated on edge node and on MP Check end to end connecitivity using barbican
    Change-Id: I79b1ebe8bf162ff00a8caa50224c73a25b858c4f

--- vmware_nsx_tempest_plugin/config.py
+++ vmware_nsx_tempest_plugin/config.py
@@ -210,3 +210,12 @@ DNSGroup = [
                default='',
                help="DNS Nameserver IP address"),
 ]
+
+barbican_group = cfg.OptGroup(name='barbican',
+                              title="barbican Configuration Options")
+
+BarbicanGroup = [
+    cfg.StrOpt('barbican_user_id',
+               default='',
+               help="barbican user id"),
+]
